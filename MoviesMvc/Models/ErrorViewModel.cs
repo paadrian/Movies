7991e9021be513b89421a40d0c388d66a190d3cc
Raylander <adrianpanaite95@gmail.com>
2022-03-11 17:45:51 +0200

    Create a web app using dotnet core mvc and simple authentication

---
+++ MoviesMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoviesMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
